3c9b1be95d668365d85c2db3b7b0911044ac647d
Kaan Ersoy <kaanersoy12@gmail.com>
2021-02-07 19:53:28 +0300

    Update app.vue for unique person selection and create giveaways

--- src/App.vue
+++ src/App.vue
@@ -3,20 +3,30 @@
     <Header></Header>
     <div class="container">
       <Description></Description>
-      <div class="form-wrapper">
-        <form @submit.prevent="apiConnection" autocomplete="off">
+      <div class="form-wrapper" v-if="!isGiveawaySuccesfull">
+        <form @submit.prevent="getComments(getVideoID())" autocomplete="off">
           <div class="form-group">
             <label>Youtube Video URL</label>
-            <input type="text">
+            <input v-model="videoUrl" @change="getVideoID" type="url" required="required">
           </div>
-          <div class="video-display">
+          <div class="video-info" v-if="isUrlAvailable">
+            <div class="box">
+              <img :src="videoInfo.thumbnails.default.url" width="150" height="120" :alt="videoInfo.title">
+            </div>
+            <div class="box">
+              <h4>
+                {{videoInfo.title}}
+              </h4>
+              <p>{{videoInfo.description.substring(0, 140)}}...</p>
+              <small><span>Video by:</span> {{videoInfo.channelTitle}}</small>
+            </div>
           </div>
           <div class="form-group">
             <label>Size of <span>Giveaway</span></label>
-            <input type="number">
+            <input type="number" v-model="giveAwaySize" required="required">
           </div>
           <div class="checkbox-group">
-            <input type="checkbox" v-model="isKeywordBasedSearchActive"  id="keyword-checkbox" name="keyword-checkbox">
+            <input type="checkbox" v-model="isKeywordBasedSearchActive" id="keyword-checkbox" name="keyword-checkbox">
             <label for="keyword-checkbox">
               <div class="slider"></div>
             </label>
@@ -31,11 +41,13 @@
           </div>
         </form>
       </div>
+      <GiveawayWinnersList></GiveawayWinnersList>
     </div>
   </div>
 </template>
 
 <script>
+import GiveawayWinnersList from './components/GiveawayWinnersList';
 import Header from './components/Header';
 import Description from './components/Description';
 import axios from 'axios';
@@ -43,23 +55,69 @@ export default {
   name: 'App',
   components: {
     Header,
-    Description
+    Description,
+    GiveawayWinnersList
   },
   data: function(){
     return{
       isKeywordBasedSearchActive: false,
-      comments: null
+      comments: null,
+      videoInfo: null,
+      videoUrl: null,
+      isUrlAvailable: null,
+      giveAwaySize: 0,
+      isGiveawaySuccesfull: false,
+      uniqueUserComments: null,
+      giveawayWinners: [],
     }
   },
   methods:{
-    apiConnection: async function(){
+    getVideoInfo: async function(videoID){
+      const apiKey = process.env.VUE_APP_YOUTUBE_API;
+      try{
+        await axios
+          .get(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet&contentDetails&statistics&id=${videoID}&key=${apiKey}`)
+          .then(res => {
+            this.videoInfo = res.data.items[0].snippet;
+          })
+          this.isUrlAvailable = true;
+      }catch(err){
+        this.isUrlAvailable = false;
+      }
+    },
+    getComments: async function(videoId){
       const apiKey = process.env.VUE_APP_YOUTUBE_API;
       axios
-      .get(`https://www.googleapis.com/youtube/v3/commentThreads?key=${apiKey}&textFormat=plainText&part=snippet&videoId=F-DNIiYyeio&maxResults=100`)
-      .then(res => {
-        console.log(res);
-        this.comments = res.data.items;
-      })
+        .get(`https://www.googleapis.com/youtube/v3/commentThreads?key=${apiKey}&textFormat=plainText&part=snippet&videoId=${videoId}&maxResults=100`)
+        .then(res => {
+          this.comments = res.data.items;
+          this.createGiveaway(this.comments);
+        })
+    },
+    getVideoID: function(){
+      const urlString = this.videoUrl;
+      try{
+        const url = new URL(urlString);
+        const videoID = url.searchParams.get('v');
+        this.getVideoInfo(videoID);
+        return videoID;
+      }catch{
+        this.isUrlAvailable = false;
+      }
+    },
+    createGiveaway: function(comments){
+      let uniqueUserComments = []
+      comments.forEach(el => {
+        const userId = el.snippet.topLevelComment.snippet.authorChannelId;
+        if(!uniqueUserComments.includes(userId)) 
+          uniqueUserComments.push(el);
+      });
+      let giveawayWinners = []
+      for (let i = 0; i < this.giveAwaySize; i++) {
+        const randomIndex = parseInt(Math.random() * uniqueUserComments.length);
+        giveawayWinners.push(uniqueUserComments[randomIndex]);
+      }
+      this.giveawayWinners = giveawayWinners;
     }
   }
 }
@@ -71,6 +129,8 @@ export default {
   /* #e7406c */
   --main-color: 231, 64, 108;
   --body-bg: 237, 240, 241;
+  /* #aaaaaa */
+  --contrasted-text: 170, 170, 170;
 }
 *{
   margin: 0;
@@ -95,6 +155,33 @@ body {
   padding: 15px;
   display: block;
 }
+.container .form-wrapper form .video-info{
+  display: flex; 
+  align-items: center;
+  margin-bottom: 10px;
+}
+.container .form-wrapper form .video-info .box{}
+.container .form-wrapper form .video-info .box:last-child{
+  margin-left: 20px;
+}
+.container .form-wrapper form .video-info .box img{}
+.container .form-wrapper form .video-info .box h4{
+  margin-bottom: 8px;
+  color: rgb(var(--main-color));
+  font-size: 18px;
+}
+.container .form-wrapper form .video-info .box p{
+  margin-bottom: 8px;
+  font-size: 15px;
+}
+.container .form-wrapper form .video-info .box small{
+  display: block;
+  font-size: 13px;
+  color: rgb(var(--contrasted-text))
+}
+.container .form-wrapper form .video-info .box small span{
+  color: rgb(var(--main-color));
+}
 .container .form-wrapper form .form-group{
   margin-bottom: 15px;
 }
@@ -109,7 +196,7 @@ body {
   color: rgb(var(--main-color));
 }
 .container .form-wrapper form .form-group label small{
-  color: #aaaaaa;
+  color: rgb(var(--contrasted-text));
 }
 .container .form-wrapper form .form-group input{ 
   width: 100%;
@@ -167,7 +254,7 @@ body {
   transform: translateX(22px);
   background-color: rgb(var(--body-bg));
 }
-.container .form-wrapper form .button-container {}
+.container .form-wrapper form .button-container{}
 .container .form-wrapper form .button-container button{
   border: none;
   box-shadow: 0 0 50px rgb(200, 200, 200);
---
+++ src/components/GiveawayWinnersList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="giveaway-information-wrapper">
+    <div class="giveaway-information-header">
+      <h5>Winner(s)</h5>
+    </div>
+    <div class="giveaway-information-content">
+      <div class="giveaway-winner-row">
+        <div class="box">
+          <img src="../assets/tombala-logo.png" width="80" height="80" alt="winnername">
+        </div>
+        <div class="box">
+          <h5>HüseyinLerbunlarlenmq</h5>
+          <p>katıldımMutluyum!!!!</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GiveawayWinnersList',
+}
+</script>
+
+<style>
+
+</style>
